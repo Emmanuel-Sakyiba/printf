7567f1e7bf016f58517c7b2233d8f4698e868578
Emmanuel-Sakyiba <manuelbusinesspro3185@gmail.com>
2023-07-24 23:56:33 -0700

    Team work is super collaboration

--- main.h
+++ main.h
@@ -15,5 +15,6 @@ int get_precision(const char *format, int *index, va_list args);
 char get_size(const char *format, int *index);
 int handle_print(const char *format, int *index, va_list args, char *buffer,
                  char flags, int width, int precision, char size);
+int printf_bin(va_list val);
 
 #endif/*MAIN_H*/
